885f7737640c4fc5c78c013b070cec179007cade
ArielRzz <99253027+ArielRzz@users.noreply.github.com>
2022-06-08 11:23:34 -0300

    agregado proyecto y modificado gitignore

---
+++ src/test/java/com/alkemy/personajesDisney/PersonajesDisneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.personajesDisney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonajesDisneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
